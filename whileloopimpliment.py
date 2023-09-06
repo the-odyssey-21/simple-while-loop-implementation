55fc139153ce31c0542204e8004f1482de575f98
the-odyssey-21 <markizie@gmail.com>
2023-09-06 21:03:02 +0300

    initial commit for whileloopimpliment

---
+++ whileloopimpliment.py
@@ -0,0 +1,10 @@
+for i in range(12):
+    print("start of month", i + 1)
+
+    number_of_days = 30
+    
+    while number_of_days <= 30:
+        print("day", number_of_days, "of month", i + 1, "is over")
+        number_of_days +=1
+    print("month", i + 1, "is over\n")
+print("the year is over")
